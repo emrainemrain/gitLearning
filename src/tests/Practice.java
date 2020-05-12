4c0ff2c6b8b3901449aa70076a6cfb2c78bea12b
emrainemrain <62119229+emrainemrain@users.noreply.github.com>
2020-05-12 16:06:38 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,5 +3,7 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("hi");
+        if(){}// this line is written from github 
+        
     }
 }
